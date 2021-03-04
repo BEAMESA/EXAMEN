d63203996be8e93aea96ada08b5f40b6debd5ec9
BEAMESA <bea.mndezsnchez@gmail.com>
2021-03-04 19:49:05 +0100

    zdthzgb

--- mapaa.js
+++ mapaa.js
@@ -2,13 +2,29 @@ var mapa;
 var tb;
 
 
-require(["esri/map",
+require([//"dojo/parser",
+        "esri/map",
         "dojo/on",
         "dojo/ready",
         "dojo/dom",
 
 
-        
+
+        "esri/geometry/Extent",
+        "esri/layers/FeatureLayer",
+        "esri/tasks/ServiceAreaParameters",
+        "esri/tasks/ServiceAreaTask",
+
+       
+        "esri/tasks/FeatureSet",
+        "esri/symbols/SimpleLineSymbol",
+        "esri/symbols/SimpleFillSymbol",
+        "esri/graphic",
+        "esri/Color",
+        "dojo/_base/array",
+        "esri/tasks/query",
+
+
 
         "dijit/layout/TabContainer",
         "dijit/layout/ContentPane",
@@ -20,19 +36,24 @@ require(["esri/map",
           ready,
           dom,
 
+          Extent, FeatureLayer, ServiceAreaParameters, ServiceAreaTask,
+
+          FeatureSet, SimpleLineSymbol, SimpleFillSymbol, Graphic, Color, arrayUtils, Query
           
 
         ) {
-    ready(function () {        
+    ready(function () {
+      
+      //parser.parse();
           
           
         mapa = new Map("map", {
           basemap: "topo",
           extent: new Extent({
-            "xmax": -7760707.875336144,
-            "xmin": -19501435.419936094,
-            "ymax": 9690521.387028327,
-            "ymin": -602183.0937376302,
+            "xmax": -404589.8111149041,
+            "xmin": -418233.82066378975,
+            "ymax": 4928456.306348966,
+            "ymin": 4918404.837129479,
             "spatialReference": {"wkid": 102100, "latestWkid": 3857}
           })
 
@@ -40,19 +61,55 @@ require(["esri/map",
 
 
 
+        var centrosSalud;
+
+        centrosSalud = new FeatureLayer ("https://services8.arcgis.com/BtkRLT3YBKaVGV3g/ArcGIS/rest/services/CentrosSalus/FeatureServer/0?token=hlzSejNkzLKqI4B_XIx1ZMqqrQcZfFPylTz_GNjGe9QcQA57CgCwWnB6cy4eoaHq9UoSRDa3xJz7W50lky-kgvxI5NXu_n-05ZQs0R_O1zop_zndGxhxEza6jluzP59fokfkSpbIO6hOMwWcLtZsoBKuICxV6KCDAzKBCN74koiz9wZcY8opCK-Xhr4pDAza25qGhCdIJFgTHCxDTW1GXhD_ustxpkHcKGDa2xij3PNoAq4xjQlVGZ-UJt-jOV72")
 
 
+        mapa.addLayers([centrosSalud]);
+
+
+        var consulta = new Query();
+        consulta.where = "1=1";
+        centrosSalud.selectFeatures(consulta, FeatureLayer.SELECTION_NEW, funcionresultado);
+
+
+        function funcionresultado (evt) {
+
+          var params = new ServiceAreaParameters ();
+          params.defaultBreaks = [3];
+          params.outSpatialReference = mapa.spatialReference;
+          params.facilities = evt;
+          params.returnPolygonBarriers = true;
+  
+  
+          console.log (params)
+
+        }
+ 
+  
+        var serviceAreaTask = new ServiceAreaTask ("https://formacion.esri.es/server/rest/services/RedMadrid/NAServer/Service%20Area");
 
         
+      
+        serviceAreaTask.solve(params,function(resultado){
+          var polygonSymbol = new SimpleFillSymbol(
+            "solid",  
+            new SimpleLineSymbol("solid", new Color([232,104,80]), 2),
+            new Color([232,104,80,0.25])
+          );
+
+          arrayUtils.forEach(resultado.serviceAreaPolygons, function(serviceArea){
+            serviceArea.setSymbol(polygonSymbol);
+            mapa.graphics.add(serviceArea);
+          })           
 
 
     });
-          
 
 
 
-}); 
+  });
 
-    
+});
 
-    
